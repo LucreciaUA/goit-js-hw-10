e1eaf3772a1a45167375560ed268389e782e812a
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2023-10-29 19:30:54 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import Notify from 'notiflix';
 import { fetchBreeds, fetchCat } from './cat-api';
 import './style.css'
@@ -9,12 +10,11 @@ const error = document.querySelector('.error');
 const cat = document.querySelector('.cat-info')
 
 loader.classList.add('is-hidden');
-error.classList.add('is-hidden')
-cat.classList.add('is-hidden')
+error.classList.add('is-hidden');
+cat.classList.add('is-hidden');
+
+select.classList.add('is-hidden')
 document.querySelector('body').insertAdjacentHTML = '<span class="loader"></span>';
-//const load = document.querySelector('.load')
-//load.classList.add('is-hidden')
-select.insertAdjacentHTML('beforeend', '<option value="" selected>None</option>');
 
 
 select.addEventListener('change', onSelect)
@@ -35,35 +35,45 @@ fetchBreeds()
         
         
         selector.setData(catArr)
-        selector.destroy()
+        //selector.insertAdjacentHTML('afterbegin', '<option value="" selected>None</option>');
+       // selector.destroy()
+        select.classList.remove('is-hidden')
+        cat.classList.add('is-hidden')
     })
+    
+
     .catch(newError)
 
 
 
 function onSelect(evt) {
-    select.classList.add('is-hidden')
+    cat.classList.add('is-hidden');
     loader.classList.remove('is-hidden')
-    cat.classList.add('is-hidden')
+    cat.innerHTML = '';
     const breadID = evt.currentTarget.value
+    
     fetchCat(breadID)
         .then(
             data => {
+                
                 const { url, breeds } = data[0]
-                //load.classList.add('is-hidden')   
+                  
     cat.innerHTML = `<img src="${url}" width='400' alt="${breeds[0].name}">
         <div><h1>'${breeds[0].name}'</h1><p>${breeds[0].description}</p>
         <p><b>Temperament: </b>${breeds[0].temperament}</p></div>`
-    cat.classList.remove('is-hidden');
-                select.classList.remove('is-hidden');
     loader.classList.add('is-hidden')
-        }
-    )
     
+            }
+            
+    )
+        .finally(
+             cat.classList.remove('is-hidden')  
+    )
     .catch(newError)
 }
 
     
 function newError() {
+    loader.classList.add('is-hidden')
     Notify.Notify.failure('Oops! Something went wrong! Try reloading the page!')
 }
